b314b73c0ee5f22fd4083d1c945b9e58bdd9f911
Gaurav <gaurav.2897@gmail.com>
2020-09-10 20:47:21 +0530

    Update create-n-read-xls.py

--- create-n-read-xls.py
+++ create-n-read-xls.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from pandas import ExcelWriter
 from pandas import ExcelFile
+import matplotlib.pyplot as plt
+
 import numpy as np
 
 df = pd.DataFrame({'a':[1,3,5,7,4,5,6,4,7,8,9],
@@ -11,6 +13,13 @@ df.to_excel(writer,'Sheet1',index=False)
 writer.save()
 
 df = pd.read_excel('Pandas.xlsx', sheet_name='Sheet1')
+
+sorted_by_gross = df.sort_values(['a'], ascending=False)
+
+sorted_by_gross['a'].head(10).plot(kind="barh")
+plt.show()
+
+
 
 print("Column headings:")
 print(df.columns)
